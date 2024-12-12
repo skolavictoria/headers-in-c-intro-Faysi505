4bf5763895e2a2da6bb73f193f27e801dd63e1d2
Stepan Hutorov <156437626+Faysi505@users.noreply.github.com>
2024-12-12 11:47:55 +0000

    G

--- Task1/main.c
+++ Task1/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "greetings.h"
+#include "greetings.c"
 
 int main(){
     void say_hello();
--- Task3/array_utils.h
+++ Task3/array_utils.h
@@ -5,5 +5,4 @@ void fill_array(int* array, int size, int value){
     for (int i = 0; i != size; i++){
         array[i] = value;
     }
-    return array;
 }
