96ca473ceb21803fd53b65794c24b45dde737ae5
Stepan Hutorov <156437626+Faysi505@users.noreply.github.com>
2024-12-18 08:04:00 +0000

    Added something

--- Task2/print_operations.h
+++ Task2/print_operations.h
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include "math_operations.h"
+#ifndef PRINT_OPERATTIONS.H
+#define PRINT_OPERATIONS.H
 
 void print_result(int result){
     printf("The result is: %d", result);
 }
+return 0;
+#endif
